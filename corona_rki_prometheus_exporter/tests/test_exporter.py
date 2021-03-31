e0582048d7aa378d3664180ebc0f3af4926aac23
Daniel Richardt <D.Richardt@t-systems.com>
2021-03-31 14:29:58 +0200

    change root path

--- corona_rki_prometheus_exporter/tests/test_exporter.py
+++ corona_rki_prometheus_exporter/tests/test_exporter.py
@@ -1,22 +1,22 @@
 from prometheus_client import Gauge  # type: ignore
-import exporter  # type: ignore
+import corona_rki_prometheus_exporter  # type: ignore
 
 
 def test_getcorona_information_from_rki():
-    response_bautzen = exporter.getcorona_information_from_rki("Bautzen")
+    response_bautzen = corona_rki_prometheus_exporter.getcorona_information_from_rki("Bautzen")
     assert len(response_bautzen["features"]) > 0
-    response_noneexisting = exporter.getcorona_information_from_rki(
+    response_noneexisting = corona_rki_prometheus_exporter.getcorona_information_from_rki(
         "Not \
         existing State"
     )
     assert len(response_noneexisting["features"]) == 0
-    response_eichsfeld = exporter.getcorona_information_from_rki("Eichsfeld")
+    response_eichsfeld = corona_rki_prometheus_exporter.getcorona_information_from_rki("Eichsfeld")
     assert len(response_eichsfeld["features"]) > 0
     assert response_eichsfeld["features"] != response_bautzen["features"]
 
 
 def test_argument_parseing():
-    args = exporter.parse_arguments(["TEST"])
+    args = corona_rki_prometheus_exporter.parse_arguments(["TEST"])
     assert args.gen == "TEST"
 
 
@@ -37,7 +37,7 @@ def test_process_request(mocker):
         Einwohner Sachsen",
     )
     mocker.patch(
-        "exporter.getcorona_information_from_rki",
+        "corona_rki_prometheus_exporter.getcorona_information_from_rki",
         return_value={
             "features": [
                 {
@@ -62,7 +62,7 @@ def test_process_request(mocker):
         (cases7_per_100k, "cases7_per_100k"),
         (cases7_bl_per_100k, "cases7_bl_per_100k"),
     ):
-        exporter.process_request(gauge[0], gauge[1])
+        corona_rki_prometheus_exporter.process_request(gauge[0], gauge[1])
     assert EWZ_BL._value._value == 1
     assert EWZ._value._value == 2
     assert cases._value._value == 3
